abca6ea7a8a0a1ba6bbafe1a9141a46464bfbe8b
AnoopKumar212 <30771424+AnoopKumar212@users.noreply.github.com>
2017-09-02 18:02:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,19 +24,37 @@ button.onclick=function()
     request.open('GET','http://anoopkumar21297.imad.hasura-app.io/counter',true);
     request.send(null);
 };
-var nameInput=document.getElementById('name');
-var nameValue=nameInput.value;
+
 var submit=document.getElementById('sub_btn');
 submit.onclick=function(){
   //make a request to the server and send a name  
+   var request=new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            //do something
+            if(request.status==200){//check if it is successful
+                    var names = request.responseText;
+                    names = JSON.parse(names);
+                    var list='';
+                    for(var i=0; i< names.length; i++){
+                        list +='<li>' + names[i] + '</li>';
+                    }
+                    var ul=document.getElementById('namelist');
+                    ul.innerHTML = list;
+            }    
+        }
+        //else do nothing
+    };
+    
+    var nameInput=document.getElementById('name');
+    var nameValue=nameInput.value;
+    //Make the request
+    request.open('GET','http://anoopkumar21297.imad.hasura-app.io/submit-name?name='+nameInput,true);
+    request.send(null);
   //capture a list of name and render it as  a list
-  var names = ['name1','name2','name3','name4'];
-  var list='';
-  for(var i=0; i< names.length; i++){
-    list +='<li>' + names[i] + '</li>';
-  }
-  var ul=document.getElementById('namelist');
-  ul.innerHTML = list;
+
 
 };
 
