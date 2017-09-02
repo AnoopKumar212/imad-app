5b09cb3a440483e3a7e08b657a4aa04cb22efc67
AnoopKumar212 <30771424+AnoopKumar212@users.noreply.github.com>
2017-09-02 16:08:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 var element= document.getElementById('text');
-element.innerHTML="new text !!";
+
 
 var img= document.getElementById('img');
 var marginLeft=0;
@@ -9,6 +9,8 @@ function moveRight(){
     marginLeft= marginLeft+10;
     img.style.marginLeft = marginLeft + 'px';
 }
+
 img.onClick= function(){
     var interval = setInterval(moveRight,50);
+    element.innerHTML="new text !!";
 };
