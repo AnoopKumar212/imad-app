c0602733ece1df1809142ccd269ddf8a62e26623
AnoopKumar212 <30771424+AnoopKumar212@users.noreply.github.com>
2017-09-02 16:54:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
 var counter=0;
 var button= document.getElementById('counter');
+button.onclick=function()
 {
     //make a request to the counter endpoint
     //capture the response and store it in a variable
