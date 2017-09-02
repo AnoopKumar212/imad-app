8e0570dda8e7ecfbbb81faa8becb9276d532cdfc
AnoopKumar212 <30771424+AnoopKumar212@users.noreply.github.com>
2017-09-02 17:54:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/ui/main.js', function (req, res) {
 
 var names=[];
 app.get('/submit-name/:name',function(req,res){
-    var name= req.paramas.name;
+    var name= req.params.name;
     names.push(name);
     res.send(JSON.stringify(names));
     
