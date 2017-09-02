82db2b9cfe2cb6ee88833c542e1f4a1621d4a24e
AnoopKumar212 <30771424+AnoopKumar212@users.noreply.github.com>
2017-09-02 16:08:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element= document.getElementById('text');
 
 
-var img= document.getElementById('img');
+var img= document.getElementById('image');
 var marginLeft=0;
 function moveRight(){
     marginLeft= marginLeft+10;
