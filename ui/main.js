509fa02909b422397657057d4a312f4e7b757451
AnoopKumar212 <30771424+AnoopKumar212@users.noreply.github.com>
2017-09-02 17:27:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,12 +17,26 @@ button.onclick=function()
             }    
         }
         //else do nothing
-    }
+    };
     
     
     //Make the request
     request.open('GET','http://anoopkumar21297.imad.hasura-app.io/counter',true);
     request.send(null);
-    
-    
-}
+};
+var nameInput=document.getElementById('name');
+var nameValue=nameInput.value;
+var submit=document.getElementById('sub_btn');
+submit.onclick=function(){
+  //make a request to the server and send a name  
+  //capture a list of name and render it as  a list
+  var names = ['name1','name2','name3','name4'];
+  var list='';
+  for(var i=0; i< names.length; i++){
+    list <='<li>' + names[i] + '</li>';
+  }
+  var ul=document.getElementById('namelist');
+  ul.innerHTML = list;
+
+};
+
