070a10ce034afbc89158ced6ba524939ba7c3ad1
AnoopKumar212 <30771424+AnoopKumar212@users.noreply.github.com>
2017-09-03 16:23:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
+
+var config={
+    user: 'anoopkumar21297',
+    database: 'anoopkumar21297',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD,
+    
+    
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -71,6 +82,19 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+    pool.query('Select * from test',function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    });
+  
 });
 
 var counter=0;
