a3a0f78e6e82f8bde685495ea6a854941d78f095
AnoopKumar212 <30771424+AnoopKumar212@users.noreply.github.com>
2017-09-03 17:02:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/article/:articleName', function (req, res) {
             else
             {
             var articleData= result.rows[0];
-            res.send(createTemplate(articleData);
+            res.send(createTemplate(articleData));
             }
                 
         }
